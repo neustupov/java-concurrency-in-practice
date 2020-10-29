7dc334ea1a56984821ecda9f0b3d6a6d6b108c66
Dmitry Neustupov <neustupov@yandex.ru>
2020-10-29 12:33:57 +0300

    добаил jcip зависимость для аннотаций

--- src/main/java/org.neustupov/StatelessFactorizedServlet.java
+++ src/main/java/org.neustupov/StatelessFactorizedServlet.java
@@ -1,5 +1,7 @@
 package org.neustupov;
 
+import net.jcip.annotations.NotThreadSafe;
+
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServlet;
@@ -8,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.math.BigInteger;
 
+@NotThreadSafe
 public class StatelessFactorizedServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1;
