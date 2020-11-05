3dc249c21e587834179c63dfe463dad8b9dfa556
Dmitry Neustupov <neustupov@yandex.ru>
2020-11-05 18:01:24 +0300

    CountDownLatch

---
+++ src/main/java/org/neustupov/synchronizers/countdownlatch/TestHarness.java
@@ -0,0 +1,33 @@
+package org.neustupov.synchronizers.countdownlatch;
+
+import java.util.concurrent.CountDownLatch;
+
+public class TestHarness {
+
+    public long timeTasks(int nThreads, final Runnable task) throws InterruptedException {
+        final CountDownLatch startGate = new CountDownLatch(1);
+        final CountDownLatch endGate = new CountDownLatch(nThreads);
+
+        for (int i = 0; i < nThreads; i++) {
+            Thread t = new Thread(() -> {
+                try {
+                    startGate.await();
+                    try {
+                        task.run();
+                    } finally {
+                        endGate.countDown();
+                    }
+                } catch (InterruptedException e) {
+                    System.out.println("Interrupted");
+                }
+            });
+            t.start();
+        }
+
+        long start = System.nanoTime();
+        startGate.countDown();
+        endGate.await();
+        long end = System.nanoTime();
+        return end - start;
+    }
+}
