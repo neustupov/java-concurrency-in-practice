ceaa17e3b42a8d097b43e5d602673db3e18c3e0e
Dmitry Neustupov <neustupov@yandex.ru>
2020-11-07 15:19:15 +0300

    Executor with shutdown

---
+++ src/main/java/org/neustupov/LifecycleWebServer.java
@@ -0,0 +1,33 @@
+package org.neustupov;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.RejectedExecutionException;
+
+public class LifecycleWebServer {
+    private final ExecutorService exec = Executors.newCachedThreadPool();
+
+    public void start() throws IOException {
+        ServerSocket socket = new ServerSocket(80);
+        while (!exec.isShutdown()) {
+            try {
+                final Socket conn = socket.accept();
+                exec.execute(() -> handleRequest(conn));
+            } catch (RejectedExecutionException e) {
+                if (!exec.isShutdown()) {
+                    System.out.println("Задача отклонена");
+                }
+            }
+        }
+    }
+
+    public void stop() {
+        exec.shutdown();
+    }
+
+    private static void handleRequest(Socket conn) {
+    }
+}
