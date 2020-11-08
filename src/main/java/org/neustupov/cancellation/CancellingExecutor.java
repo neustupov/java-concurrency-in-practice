edd26d5bbec8fe153cc6babea67864c9d9427d63
Dmitry Neustupov <neustupov@yandex.ru>
2020-11-08 15:06:03 +0300

    Остановка поточной службы

---
+++ src/main/java/org/neustupov/cancellation/CancellingExecutor.java
@@ -0,0 +1,33 @@
+package org.neustupov.cancellation;
+
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.concurrent.*;
+
+@ThreadSafe
+public class CancellingExecutor extends ThreadPoolExecutor {
+
+    public CancellingExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue) {
+        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);
+    }
+
+    public CancellingExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue, ThreadFactory threadFactory) {
+        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, threadFactory);
+    }
+
+    public CancellingExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue, RejectedExecutionHandler handler) {
+        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, handler);
+    }
+
+    public CancellingExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue, ThreadFactory threadFactory, RejectedExecutionHandler handler) {
+        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, threadFactory, handler);
+    }
+
+    protected <T> RunnableFuture newTaskFor(Callable<T> callable) {
+        if (callable instanceof CancellableTask) {
+            return ((CancellableTask) callable).newTask();
+        } else {
+            return super.newTaskFor(callable);
+        }
+    }
+}
