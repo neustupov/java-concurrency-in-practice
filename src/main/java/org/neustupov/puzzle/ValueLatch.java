0a28bac11101191e4ead24b2860c9cf8f1ad9068
Dmitry Neustupov <neustupov@yandex.ru>
2020-11-18 16:46:00 +0300

    Применение пула потоков

---
+++ src/main/java/org/neustupov/puzzle/ValueLatch.java
@@ -0,0 +1,32 @@
+package org.neustupov.puzzle;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.concurrent.CountDownLatch;
+
+@ThreadSafe
+public class ValueLatch<T> {
+
+    @GuardedBy("this")
+    private T value = null;
+    private final CountDownLatch done = new CountDownLatch(1);
+
+    public boolean isSet() {
+        return (done.getCount() == 0);
+    }
+
+    public synchronized void setValue(T newValue) {
+        if (!isSet()) {
+            value = newValue;
+            done.countDown();
+        }
+    }
+
+    public T getValue() throws InterruptedException {
+        done.await();
+        synchronized (this) {
+            return value;
+        }
+    }
+}
