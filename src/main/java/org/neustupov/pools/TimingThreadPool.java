95358982127122ba18888a33413380a065003ab6
Dmitry Neustupov <neustupov@yandex.ru>
2020-11-18 13:55:50 +0300

    Росширение пула потоков

---
+++ src/main/java/org/neustupov/pools/TimingThreadPool.java
@@ -0,0 +1,48 @@
+package org.neustupov.pools;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+public class TimingThreadPool extends ThreadPoolExecutor {
+
+    private final ThreadLocal<Long> startTime = new ThreadLocal<>();
+    private final Logger log = Logger.getLogger("TimingThreadPool");
+    private final AtomicLong numTasks = new AtomicLong();
+    private final AtomicLong totalTime = new AtomicLong();
+
+    public TimingThreadPool(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue) {
+        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);
+    }
+
+    @Override
+    protected void beforeExecute(Thread t, Runnable r) {
+        super.beforeExecute(t, r);
+        log.fine(String.format("Поток %s: начало %s", t, r));
+        startTime.set(System.nanoTime());
+    }
+
+    @Override
+    protected void afterExecute(Runnable r, Throwable t) {
+        try {
+            long endTime = System.nanoTime();
+            long taskTime = endTime - startTime.get();
+            numTasks.incrementAndGet();
+            totalTime.addAndGet(taskTime);
+            log.fine(String.format("Поток %s: конец %s, время %dns", t, r, taskTime));
+        } finally {
+            super.afterExecute(r, t);
+        }
+    }
+
+    @Override
+    protected void terminated() {
+        try {
+            log.info(String.format("Terminated: avg time =%dns", totalTime.get() / numTasks.get()));
+        } finally {
+            super.terminated();
+        }
+    }
+}
