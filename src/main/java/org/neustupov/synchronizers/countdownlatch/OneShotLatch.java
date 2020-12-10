7ed72130e5c9c01fce369ca57321acb7b5a84c96
Dmitry Neustupov <neustupov@yandex.ru>
2020-12-10 16:26:30 +0300

    AQS

---
+++ src/main/java/org/neustupov/synchronizers/countdownlatch/OneShotLatch.java
@@ -0,0 +1,32 @@
+package org.neustupov.synchronizers.countdownlatch;
+
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+
+@ThreadSafe
+public class OneShotLatch {
+
+    private final Sync sync = new Sync();
+
+    public void signal() {
+        sync.releaseShared(0);
+    }
+
+    public void await() throws InterruptedException {
+        sync.acquireSharedInterruptibly(0);
+    }
+
+    private class Sync extends AbstractQueuedSynchronizer {
+        @Override
+        protected int tryAcquireShared(int arg) {
+            return (getState() == 1) ? 1 : -1;
+        }
+
+        @Override
+        protected boolean tryReleaseShared(int arg) {
+            setState(1);
+            return true;
+        }
+    }
+}
