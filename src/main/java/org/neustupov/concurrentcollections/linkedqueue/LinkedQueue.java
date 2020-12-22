cf7073f49f3c319983b53a037e78f8c98d449fb4
Dmitry_Neustupov <neustupov@yandex.ru>
2020-12-22 17:31:51 +0300

    Michael-Scott alg

---
+++ src/main/java/org/neustupov/concurrentcollections/linkedqueue/LinkedQueue.java
@@ -0,0 +1,41 @@
+package org.neustupov.concurrentcollections.linkedqueue;
+
+import java.util.concurrent.atomic.AtomicReference;
+import net.jcip.annotations.ThreadSafe;
+
+@ThreadSafe
+public class LinkedQueue<E> {
+
+  private static class Node<E> {
+
+    final E item;
+    final AtomicReference<Node<E>> next;
+
+    public Node(E item, Node<E> next) {
+      this.item = item;
+      this.next = new AtomicReference<>(next);
+    }
+  }
+
+  private final Node<E> dummy = new Node<>(null, null);
+  private final AtomicReference<Node<E>> head = new AtomicReference<>(dummy);
+  private final AtomicReference<Node<E>> tail = new AtomicReference<>(dummy);
+
+  public boolean put(E item) {
+    Node<E> newNode = new Node<>(item, null);
+    while (true) {
+      Node<E> curTail = tail.get();
+      Node<E> tailNext = curTail.next.get();
+      if (curTail == tail.get()) {
+        if (tailNext != null) {
+          tail.compareAndSet(curTail, tailNext);
+        } else {
+          if (curTail.next.compareAndSet(null, newNode)) {
+            tail.compareAndSet(curTail, newNode);
+            return true;
+          }
+        }
+      }
+    }
+  }
+}
